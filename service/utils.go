800a2601c7583e9a7d0f823dc77eedbd52b1f5fa
Vaclav Rozsypalek <rozsypalek.vaclav@gmail.com>
2018-05-23 15:46:40 +0200

    request changes

--- service/utils.go
+++ service/utils.go
@@ -81,38 +81,43 @@ func CreateCRDClient(logger micrologger.Logger) (*versioned.Clientset, error) {
 func CheckClusterVersionSupport(clusterID string, provider string, crdCLient *versioned.Clientset) (bool, error) {
 	getOpts := metav1.GetOptions{}
 
-	if provider == aws {
-		crd, err := crdCLient.ProviderV1alpha1().AWSConfigs(crdNamespace).Get(clusterID, getOpts)
-		if err != nil {
-			return false, microerror.Maskf(err, "failed to get aws crd"+clusterID)
+	switch provider {
+	case aws:
+		{
+			crd, err := crdCLient.ProviderV1alpha1().AWSConfigs(crdNamespace).Get(clusterID, getOpts)
+			if err != nil {
+				return false, microerror.Maskf(err, "failed to get aws crd"+clusterID)
+			}
+			crdVersion := semver.New(crd.Spec.VersionBundle.Version)
+			if crdVersion.Compare(*awsSupportFrom) >= 0 {
+				// version has support
+				return true, nil
+			} else {
+				// version doesnt have support
+				return false, nil
+			}
 		}
-		crdVersion := semver.New(crd.Spec.VersionBundle.Version)
-		if crdVersion.Compare(*awsSupportFrom) >= 0 {
-			// version has support
+	case azure:
+		{
+			crd, err := crdCLient.ProviderV1alpha1().AzureConfigs(crdNamespace).Get(clusterID, getOpts)
+			if err != nil {
+				return false, microerror.Maskf(err, "failed to get azure crd "+clusterID)
+			}
+
+			crdVersion := semver.New(crd.Spec.VersionBundle.Version)
+			if crdVersion.Compare(*azureSupportFrom) >= 0 {
+				// version has support
+				return true, nil
+			} else {
+				// version doesnt have support
+				return false, nil
+			}
+		}
+	case kvm:
+		{
+			// kvm backups are always supported
 			return true, nil
-		} else {
-			// version doesnt have support
-			return false, nil
 		}
-
-	} else if provider == azure {
-		crd, err := crdCLient.ProviderV1alpha1().AzureConfigs(crdNamespace).Get(clusterID, getOpts)
-		if err != nil {
-			return false, microerror.Maskf(err, "failed to get azure crd "+clusterID)
-		}
-
-		crdVersion := semver.New(crd.Spec.VersionBundle.Version)
-		if crdVersion.Compare(*azureSupportFrom) >= 0 {
-			// version has support
-			return true, nil
-		} else {
-			// version doesnt have support
-			return false, nil
-		}
-
-	} else if provider == kvm {
-		// kvm backups are always supported
-		return true, nil
 	}
 	return false, nil
 }
@@ -122,34 +127,44 @@ func GetAllGuestClusters(provider string, crdCLient *versioned.Clientset) ([]str
 	var clusterList []string
 	listOpt := metav1.ListOptions{}
 
-	if provider == aws {
-		crdList, err := crdCLient.ProviderV1alpha1().AWSConfigs(crdNamespace).List(listOpt)
-		if err != nil {
-			return []string{}, microerror.Maskf(err, "failed to list aws crd")
+	switch provider {
+	case aws:
+		{
+			crdList, err := crdCLient.ProviderV1alpha1().AWSConfigs(crdNamespace).List(listOpt)
+			if err != nil {
+				return []string{}, microerror.Maskf(err, "failed to list aws crd")
+			}
+			for _, awsConfig := range crdList.Items {
+				clusterList = append(clusterList, awsConfig.Name)
+			}
+			break
 		}
-		for _, awsConfig := range crdList.Items {
-			clusterList = append(clusterList, awsConfig.Name)
+	case azure:
+		{
+			crdList, err := crdCLient.ProviderV1alpha1().AzureConfigs(crdNamespace).List(listOpt)
+			if err != nil {
+				return []string{}, microerror.Maskf(err, "failed to list azure crd")
+			}
+			for _, azureConfig := range crdList.Items {
+				clusterList = append(clusterList, azureConfig.Name)
+			}
+			break
 		}
-
-	} else if provider == azure {
-		crdList, err := crdCLient.ProviderV1alpha1().AzureConfigs(crdNamespace).List(listOpt)
-		if err != nil {
-			return []string{}, microerror.Maskf(err, "failed to list azure crd")
+	case kvm:
+		{
+			crdList, err := crdCLient.ProviderV1alpha1().KVMConfigs(crdNamespace).List(listOpt)
+			if err != nil {
+				return []string{}, microerror.Maskf(err, "failed to list azure crd")
+			}
+			for _, kvmConfig := range crdList.Items {
+				clusterList = append(clusterList, kvmConfig.Name)
+			}
+			break
 		}
-		for _, azureConfig := range crdList.Items {
-			clusterList = append(clusterList, azureConfig.Name)
+	default:
+		{
+			return []string{}, invalidProviderError
 		}
-
-	} else if provider == kvm {
-		crdList, err := crdCLient.ProviderV1alpha1().KVMConfigs(crdNamespace).List(listOpt)
-		if err != nil {
-			return []string{}, microerror.Maskf(err, "failed to list azure crd")
-		}
-		for _, kvmConfig := range crdList.Items {
-			clusterList = append(clusterList, kvmConfig.Name)
-		}
-	} else {
-		return []string{}, microerror.Mask(invalidProviderError)
 	}
 
 	return clusterList, nil
@@ -158,16 +173,16 @@ func GetAllGuestClusters(provider string, crdCLient *versioned.Clientset) ([]str
 // fetch etcd client certs
 func FetchCerts(clusterID string, k8sClient kubernetes.Interface) (*k8sclient.TLSClientConfig, error) {
 
-	certs := &k8sclient.TLSClientConfig{}
-	{
-		getOpts := metav1.GetOptions{}
-		secret, err := k8sClient.CoreV1().Secrets(secretNamespace).Get(clusterID+"-etcd", getOpts)
-		if err != nil {
-			return nil, microerror.Maskf(err, "error getting etcd client certificates for guest cluster %s", clusterID)
-		}
-		certs.CAData = secret.Data["ca"]
-		certs.KeyData = secret.Data["key"]
-		certs.CrtData = secret.Data["crt"]
+	getOpts := metav1.GetOptions{}
+	secret, err := k8sClient.CoreV1().Secrets(secretNamespace).Get(clusterID+"-etcd", getOpts)
+	if err != nil {
+		return nil, microerror.Maskf(err, "error getting etcd client certificates for guest cluster %s", clusterID)
+	}
+
+	certs := &k8sclient.TLSClientConfig{
+		CAData:  secret.Data["ca"],
+		KeyData: secret.Data["key"],
+		CrtData: secret.Data["crt"],
 	}
 
 	return certs, nil
@@ -177,34 +192,41 @@ func FetchCerts(clusterID string, k8sClient kubernetes.Interface) (*k8sclient.TL
 func GetEtcdEndpoint(clusterID string, provider string, crdCLient *versioned.Clientset) (string, error) {
 	getOpts := metav1.GetOptions{}
 	var etcdEndpoint string
-	if provider == aws {
-		crd, err := crdCLient.ProviderV1alpha1().AWSConfigs(crdNamespace).Get(clusterID, getOpts)
-		if err != nil {
-			fmt.Println()
-			return "", microerror.Maskf(err, "error getting aws crd for guest cluster %s", clusterID)
-		}
-		etcdEndpoint = AwsEtcdEndpoint(crd.Spec.Cluster.Etcd.Domain)
 
-	} else if provider == azure {
-		crd, err := crdCLient.ProviderV1alpha1().AzureConfigs(crdNamespace).Get(clusterID, getOpts)
-		if err != nil {
-			fmt.Println()
-			return "", microerror.Maskf(err, "error getting azure crd for guest cluster %s", clusterID)
+	switch provider {
+	case aws:
+		{
+			crd, err := crdCLient.ProviderV1alpha1().AWSConfigs(crdNamespace).Get(clusterID, getOpts)
+			if err != nil {
+				fmt.Println()
+				return "", microerror.Maskf(err, "error getting aws crd for guest cluster %s", clusterID)
+			}
+			etcdEndpoint = AwsEtcdEndpoint(crd.Spec.Cluster.Etcd.Domain)
+			break
 		}
-		etcdEndpoint = AzureEtcdEndpoint(crd.Spec.Cluster.Etcd.Domain)
-
-	} else if provider == kvm {
-		crd, err := crdCLient.ProviderV1alpha1().KVMConfigs(crdNamespace).Get(clusterID, getOpts)
-		if err != nil {
-			fmt.Println()
-			return "", microerror.Maskf(err, "error getting kvm crd for guest cluster %s", clusterID)
+	case azure:
+		{
+			crd, err := crdCLient.ProviderV1alpha1().AzureConfigs(crdNamespace).Get(clusterID, getOpts)
+			if err != nil {
+				fmt.Println()
+				return "", microerror.Maskf(err, "error getting azure crd for guest cluster %s", clusterID)
+			}
+			etcdEndpoint = AzureEtcdEndpoint(crd.Spec.Cluster.Etcd.Domain)
+			break
 		}
-		etcdEndpoint = KVMEtcdEndpoint(crd.Spec.Cluster.Etcd.Domain)
-
+	case kvm:
+		{
+			crd, err := crdCLient.ProviderV1alpha1().KVMConfigs(crdNamespace).Get(clusterID, getOpts)
+			if err != nil {
+				fmt.Println()
+				return "", microerror.Maskf(err, "error getting kvm crd for guest cluster %s", clusterID)
+			}
+			etcdEndpoint = KVMEtcdEndpoint(crd.Spec.Cluster.Etcd.Domain)
+			break
+		}
 	}
 
 	// we already check for unknown provider at the start
-
 	return etcdEndpoint, nil
 }
 
